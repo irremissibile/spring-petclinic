d634a3b288a87d5bbeaa0010f772324e2694d567
self-affected <ihnatov.d@gmail.com>
2020-06-26 12:37:30 +0300

    Owners list updated

--- src/main/java/co/winish/petclinic/controllers/OwnerController.java
+++ src/main/java/co/winish/petclinic/controllers/OwnerController.java
@@ -1,5 +1,6 @@
 package co.winish.petclinic.controllers;
 
+import co.winish.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping(path = {"", "/", "/index", "/index.html"})
     public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
