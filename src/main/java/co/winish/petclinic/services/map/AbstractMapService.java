af15ddaf68091bda94bd07b5543b85d87541697f
self-affected <ihnatov.d@gmail.com>
2020-06-26 12:53:45 +0300

    Modified AbstractMapService

--- src/main/java/co/winish/petclinic/services/map/AbstractMapService.java
+++ src/main/java/co/winish/petclinic/services/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package co.winish.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import co.winish.petclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +16,14 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T t) {
-        map.put(id, t);
+    T save(T t) {
+        if (t != null) {
+            if (t.getId() == null)
+                t.setId(getNextId());
+            map.put(t.getId(), t);
+        } else
+            throw new RuntimeException("Object cannot be null");
+
         return t;
     }
 
@@ -28,5 +33,15 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T t) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(t));
+    }
+
+    private Long getNextId() {
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
