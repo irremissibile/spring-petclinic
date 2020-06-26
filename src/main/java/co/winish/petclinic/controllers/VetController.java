47744026034e7e73a24fd47512dc06be3bfeb078
self-affected <ihnatov.d@gmail.com>
2020-06-26 12:42:25 +0300

    Vets list updated

--- src/main/java/co/winish/petclinic/controllers/VetController.java
+++ src/main/java/co/winish/petclinic/controllers/VetController.java
@@ -1,5 +1,6 @@
 package co.winish.petclinic.controllers;
 
+import co.winish.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping(path = {"", "/", "/index", "/index.html"})
     public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
