cc2889a873043e23a9a7cd719905a155fdc1020a
self-affected <ihnatov.d@gmail.com>
2020-06-30 21:38:33 +0300

    Updated JPA entities

---
+++ src/main/java/co/winish/petclinic/model/Specialty.java
@@ -0,0 +1,21 @@
+package co.winish.petclinic.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table("specialties")
+public class Specialty extends BaseEntity {
+
+    @Column(name = "description")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
